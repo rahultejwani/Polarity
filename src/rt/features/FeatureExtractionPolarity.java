fb9120168eeb3ffb31ef1122eb612e3ce001aae4
rahultejwani <rahul.tejwani03@gmail.com>
2014-04-05 18:36:41 -0400

    Removed Warnings

--- src/rt/features/FeatureExtractionPolarity.java
+++ src/rt/features/FeatureExtractionPolarity.java
@@ -1,9 +1,5 @@
 
 package rt.features;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.HashMap;
